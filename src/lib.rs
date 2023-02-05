c3f52d5485fd8aba22e6467ef4b3383930b933b0
一丝 <percyley@qq.com>
2023-02-05 16:57:15 +0800

    lint: run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 use napi::bindgen_prelude::Buffer;
 use napi_derive::napi;
 
-use woff2::decode::{convert_woff2_to_ttf};
+use woff2::decode::convert_woff2_to_ttf;
 
 #[napi]
 pub fn decode(input: Buffer) -> Buffer {
